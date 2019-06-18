f1f4c2c5d103548df3253ee26501085ff6a2d577
levantungb <le.van.tungb@sun-asterisk>
2019-06-18 10:49:09 +0700

    manager-category-list-delete

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,16 +1,36 @@
 class CategoriesController < AdminsController
-  def index; end
+  before_action :load_category, only: %i(destroy)
+
+  def index
+    @categories = Category.page(params[:page]).per(Settings.category_per_page)
+      .order_by_column :name
+  end
 
   def new; end
 
   def create
     @category = Category.new category_params
     if @category.save
-      flash[:success] = t "store.create_success"
+      flash[:success] = t "category.delete_success"
       redirect_to stores_url
     else
       render :new
     end
+  end
+
+  def destroy
+    if @category.destroy
+      flash[:success] = t "category.delete_success"
+    else
+      flash[:danger] = t "category.some_thing_was_wrong"
+    end
+    redirect_to categories_url
+  end
+
+  def load_category
+    @category = Category.find_by id: params[:id]
+    return if @category
+    render file: "#{Rails.root}/public/404", status: :not_found
   end
 
   private
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :store
   has_many :products
+  scope :order_by_column, ->(column){order(column)}
 
   CATEGORY_PARAMS = %i(name description store_id).freeze
+
+  delegate :name, to: :store, prefix: true
 end
