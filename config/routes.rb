1fad1ebb22059561f58512b014899ce4f629f289
thangtv-1934 <tran.van.thang@sun-asterisk.com>
2019-06-21 09:15:00 +0700

    list product with category

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,8 @@ Rails.application.routes.draw do
 
   get "/login", to:"sessions#new"
   get "static_pages/index"
-  get "/search", to:"stores#search"
+  get "/search", to:"products#search"
+  get "search_product_cate/:id", to: "products#search_by_cate", as: "search_by_cate"
   get "/store/:id", to: "stores#detail", as: "store_detail"
 
   post "/login", to:"sessions#create"
